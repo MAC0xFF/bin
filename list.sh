6dc7ad3498b75d58c1926fb42b472ae62934be01
MAC0xFF <amicus.antonius.felix@gmail.com>
2025-06-21 00:08:48 +0300

    Create list.sh

---
+++ list.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+function Usage_Exit {
+  echo "$0 [color|last|len|long]"; exit
+}
+function Ls-Length {
+  ls -1 "$@" | while read fn; do
+    printf '%3d %s\n' ${#fn} ${fn}; done | sort -n
+}
+(( $# < 1 )) && Usage_Exit 
+sub=$1
+shift
+case $sub in
+  color) ls -N --color=tty -T 0 "$@" ;;
+  last) ls -lrt | tail "-n${1:-5}" ;;
+  len*) Ls-Length "$@" ;;
+  long) Ls-Length "$@" | tail -1 ;;
+  *) echo "unknown command: $sub"; Usage_Exit ;;
+esac
